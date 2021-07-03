86b538255209cc5cd73baa528a827d96267dec98
Po-Han Chen <pohan@caddyxtra.stanford.edu>
2021-07-02 21:33:01 -0700

    update SoC simulation

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# uncomment to install caravel (only need in first run)
+# make install
+
+export OPENLANE_ROOT=/tmp/pohan/openlane
+export OPENLANE_TAG=v0.15
+export CARAVEL_ROOT=$(pwd)/caravel
+export PRECHECK_ROOT=/tmp/pohan/precheck
+export GCC_PATH=/tmp/pohan/riscv/bin
+export PDK_ROOT=/tmp/pohan/
+export PDK_PATH=/tmp/pohan/sky130A
+
